81b78967fc9d832318cc16160a79cb7ea2a640f1
Krishna Nagpure <71550035+Krishna12-1997@users.noreply.github.com>
2022-01-09 10:54:14 +0530

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+const cityName = document.getElementById('cityName');
+const submitbtn = document.getElementById('submitbtn');
+
+const city_name = document.getElementById('city_name');
+const  temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+
+const datahide = document.querySelector('.middle_layer');
+
+const getInfo = async(event) =>{
+    event.preventDefault();
+    let cityVal = cityName.value;
+    if(cityVal == ""){
+        city_name.innerText = 'Plz write the name before search';
+        datahide.classList.add('data_hide');
+        
+    }else{
+       try{
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=cd76f2f65d383a1b164dbbdd5be227b9`
+        const response = await fetch(url);
+        const data = await response.json();
+        const arrData = [data];
+        
+        city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+        temp_real_val.innerText = arrData[0].main.temp;
+
+         const tempMood = arrData[0].weather[0].main;
+
+         // condition weather to be sunny or cloudy
+         if(tempMood == "clear"){
+            temp_status.innerHTML = "<i class='fa fa-sun' style: 'color: #eccc68 </i>";
+         } else if(tempMood == "clouds"){
+            temp_status.innerHTML = "<i class='fa fa-cloud' style: 'color: #f1f2f6 </i>";
+         } else if(tempMood == "Rain"){
+            temp_status.innerHTML = "<i class='fa fa-cloud-Rain' style: 'color: #a4b0be </i>";
+         } else {
+            temp_status.innerHTML = "<i class='fa fa-sun' style: 'color: #eccc68 </i>";
+         }
+
+         datahide.classList.remove('data_hide');
+
+       } catch{
+        city_name.innerText = 'Plz enter your city name properly';
+        datahide.classList.add('data_hide');
+       }
+    }
+}
+
+submitbtn.addEventListener('click', getInfo);
